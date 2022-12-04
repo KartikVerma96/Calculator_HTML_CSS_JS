a646de677d1b2e5b18bc052c7507c3cdd27b0593
KartikVerma96 <113691568+KartikVerma96@users.noreply.github.com>
2022-12-05 00:18:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//get number
+function number(num){
+    var result = document.getElementById("input");
+    result.value += num;
+} 
+
+//get result
+
+function result(){
+    var result = document.getElementById("input");
+    result.value = eval(result.value)
+}
+
+//clear result
+
+function clearResult(){
+    var result = document.getElementById("input");
+    result.value = "";
+}
+
+function deleteChar(){
+    var number = document.getElementById("input");
+    var remove = number.value;
+    remove = remove.slice(0,-1);    //remove last character from a string
+    number.value = remove;
+}
